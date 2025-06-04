e18147049af75f9ba67d81286e7260c3e41b5593
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2025-06-04 13:03:06 +0200

    Added user and role controllers, migrations, login models; updated basket and customer services

---
+++ Migrations/20250518070924_Added stock.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hoved_Opgave_Datamatiker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedstock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Stock",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stock",
+                table: "Products");
+        }
+    }
+}
---
+++ Migrations/20250526102839_new property.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hoved_Opgave_Datamatiker.Migrations
+{
+    /// <inheritdoc />
+    public partial class newproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Products",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Products");
+        }
+    }
+}
